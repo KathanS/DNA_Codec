8995e2810c1faecd2f5734f8307556ff3640a525
Ayush Prajapati <58745662+ayushprajapati@users.noreply.github.com>
2022-11-21 21:55:49 +0530

    Add files via upload

---
+++ Huffman/huffman_decoding.py
@@ -0,0 +1,17 @@
+def Huffman_Decoding(encoded_data, huffman_tree):
+    tree_head = huffman_tree
+    decoded_output = []
+    for x in encoded_data:
+        if x == '1':
+            huffman_tree = huffman_tree.right   
+        elif x == '0':
+            huffman_tree = huffman_tree.left
+        try:
+            if huffman_tree.left.symbol == None and huffman_tree.right.symbol == None:
+                pass
+        except AttributeError:
+            decoded_output.append(huffman_tree.symbol)
+            huffman_tree = tree_head
+        
+    string = ''.join([str(item) for item in decoded_output])
+    return string
