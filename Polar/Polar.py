051802683bf5984621a06170b82d39fa1d914b0f
unknown <201901026@daiict.ac.in>
2022-11-23 00:58:13 +0530

    Polar.py updated

--- Polar/Polar.py
+++ Polar/Polar.py
@@ -10,7 +10,7 @@ def encode(bits):
     # bits: input bits
     # return: encoded bits
     input_text= bits
-    [encoded_output,decoded_output] = eng.codecPolar(matlab.int8(input_text),nargout=2)
+    encoded_output = eng.encodePolar(matlab.int8(input_text),nargout=1)
     encoded_output =[[int(num) for num in x] for x in encoded_output]
     print(encoded_output)
     eng.quit()
@@ -21,8 +21,11 @@ def decode(bits):
     # Polar decoding
     # bits: input bits
     # return: decoded bits
-    import matlab.engine
     eng = matlab.engine.start_matlab()
-    eng.cd(r'C:\Users\Admin\DNA_Codec\Polar', nargout=0)
-    eng.decoder(bits)
+    input_text= bits
+    decoded_output = eng.decodePolar(matlab.int8(input_text),nargout=1)
+    decoded_output =[[int(num) for num in x] for x in decoded_output]
+    print(decoded_output)
     eng.quit()
+    return decoded_output
+    
